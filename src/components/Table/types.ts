454f2f2f185edbdb73398e542a6b2045621a3551
Sonja Feitsch <feitsch@e2n.de>
2023-01-23 15:26:49 +0100

    chore: update typings

--- src/components/Table/types.ts
+++ src/components/Table/types.ts
@@ -1,9 +1,15 @@
 import { ColumnDef as ReactTableColumnDef } from '@tanstack/react-table';
-import { SortingState as ReactSortingState } from '@tanstack/react-table';
+import { SortingState as ReactTableSortingState } from '@tanstack/react-table';
+import { Row as ReactTableRow } from '@tanstack/react-table';
+import { PaginationState as ReactTablePaginationState } from '@tanstack/react-table';
 
 export type ColumnDef<TData, TValue = unknown> = ReactTableColumnDef<
   TData,
   TValue
 >;
 
-export type SortingState = ReactSortingState;
+export type SortingState = ReactTableSortingState;
+
+export type Row<TData> = ReactTableRow<TData>;
+
+export type PaginationState = ReactTablePaginationState;
